d6a18267b0c5896f8230f10ae5946d28d4b9a6dd
airesumebuilderproject <airesumebuilderproject@gmail.com>
2025-03-08 21:44:33 +0530

    New Changes Updated

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,5 +1,14 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI();
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // API key environment variable se le rahe hain
+});
 
 export default openai;
+
+
+// import OpenAI from "openai";
+
+// const openai = new OpenAI();
+
+// export default openai;

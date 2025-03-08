f77780d66f9e8997093741a7c4e9d77ab88161ed
airesumebuilderproject <airesumebuilderproject@gmail.com>
2025-03-08 21:01:00 +0530

    New AI Updated

--- src/app/(main)/editor/actions.ts
+++ src/app/(main)/editor/actions.ts
@@ -1,100 +1,131 @@
 "use server";
 
-import prisma from "@/lib/prisma";
-import { resumeSchema, ResumeValues } from "@/lib/validation";
-import { auth } from "@clerk/nextjs/server";
-import { del, put } from "@vercel/blob";
-import path from "path";
+import openai from "@/lib/openai";
+import {
+  GenerateSummaryInput,
+  generateSummarySchema,
+  GenerateWorkExperienceInput,
+  generateWorkExperienceSchema,
+  WorkExperience,
+} from "@/lib/validation";
 
-export async function saveResume(values: ResumeValues) {
-  const { id } = values;
+export async function generateSummary(input: GenerateSummaryInput) {
+  // TODO: Block for non-premium users
 
-  console.log("\n\nReceived values :", values);
+  const { jobTitle, workExperiences, educations, skills } =
+    generateSummarySchema.parse(input);
 
-  const { photo, workExperiences, educations, ...resumeValues } =
-    resumeSchema.parse(values);
+  const systemMessage = `
+    You are a job resume generator AI. Your task is to write a professional introduction summary for a resume given the user's provided data.
+    Only return the summary and do not include any other information in the response. Keep it concise and professional.
+    `;
 
-  const { userId } = await auth();
+  const userMessage = `
+    Please generate a professional resume summary from this data:
 
-  if (!userId) {
-    throw new Error("User Not Authenticated");
-  }
+    Job title: ${jobTitle || "N/A"}
 
-  // TODO: Check resume count for non-premium users
+    Work experience:
+    ${workExperiences
+      ?.map(
+        (exp) => `
+        Position: ${exp.position || "N/A"} at ${exp.company || "N/A"} from ${exp.startDate || "N/A"} to ${exp.endDate || "Present"}
 
-  const existingResume = id
-    ? await prisma.resume.findUnique({ where: { id, userId } })
-    : null;
+        Description:
+        ${exp.description || "N/A"}
+        `,
+      )
+      .join("\n\n")}
 
-  if (id && !existingResume) {
-    throw new Error("Resume Not Found");
-  }
+      Education:
+    ${educations
+      ?.map(
+        (edu) => `
+        Degree: ${edu.degree || "N/A"} at ${edu.school || "N/A"} from ${edu.startDate || "N/A"} to ${edu.endDate || "N/A"}
+        `,
+      )
+      .join("\n\n")}
 
-  let newPhotoUrl: string | undefined | null = undefined;
+      Skills:
+      ${skills}
+    `;
 
-  if (photo instanceof File) {
-    if (existingResume?.photoUrl) {
-      await del(existingResume.photoUrl);
-    }
+  console.log("systemMessage", systemMessage);
+  console.log("userMessage", userMessage);
 
-    const blob = await put(`resume_photos/${path.extname(photo.name)}`, photo, {
-      access: "public",
-    });
-
-    newPhotoUrl = blob.url;
-  } else if (photo === null) {
-    if (existingResume?.photoUrl) {
-      await del(existingResume.photoUrl);
-    }
-    newPhotoUrl = null;
-  }
-
-  if (id) {
-    return prisma.resume.update({
-      where: { id },
-      data: {
-        ...resumeValues,
-        photoUrl: newPhotoUrl,
-        workExperiences: {
-          deleteMany: {},
-          create: workExperiences?.map((exp) => ({
-            ...exp,
-            startDate: exp.startDate ? new Date(exp.startDate) : undefined,
-            endDate: exp.endDate ? new Date(exp.endDate) : undefined,
-          })),
-        },
-        educations: {
-          deleteMany: {},
-          create: educations?.map((edu) => ({
-            ...edu,
-            startDate: edu.startDate ? new Date(edu.startDate) : undefined,
-            endDate: edu.endDate ? new Date(edu.endDate) : undefined,
-          })),
-        },
-        updatedAt: new Date(),
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content: systemMessage,
       },
-    });
-  } else {
-    return prisma.resume.create({
-      data: {
-        ...resumeValues,
-        userId,
-        photoUrl: newPhotoUrl,
-        workExperiences: {
-          create: workExperiences?.map((exp) => ({
-            ...exp,
-            startDate: exp.startDate ? new Date(exp.startDate) : undefined,
-            endDate: exp.endDate ? new Date(exp.endDate) : undefined,
-          })),
-        },
-        educations: {
-          create: educations?.map((edu) => ({
-            ...edu,
-            startDate: edu.startDate ? new Date(edu.startDate) : undefined,
-            endDate: edu.endDate ? new Date(edu.endDate) : undefined,
-          })),
-        },
+      {
+        role: "user",
+        content: userMessage,
       },
-    });
+    ],
+  });
+
+  const aiResponse = completion.choices[0].message.content;
+
+  if (!aiResponse) {
+    throw new Error("Failed to generate AI response");
   }
+
+  return aiResponse;
+}
+
+export async function generateWorkExperience(
+  input: GenerateWorkExperienceInput,
+) {
+  // TODO: Block for non-premium users
+
+  const { description } = generateWorkExperienceSchema.parse(input);
+
+  const systemMessage = `
+  You are a job resume generator AI. Your task is to generate a single work experience entry based on the user input.
+  Your response must adhere to the following structure. You can omit fields if they can't be infered from the provided data, but don't add any new ones.
+
+  Job title: <job title>
+  Company: <company name>
+  Start date: <format: YYYY-MM-DD> (only if provided)
+  End date: <format: YYYY-MM-DD> (only if provided)
+  Description: <an optimized description in bullet format, might be infered from the job title>
+  `;
+
+  const userMessage = `
+  Please provide a work experience entry from this description:
+  ${description}
+  `;
+
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content: systemMessage,
+      },
+      {
+        role: "user",
+        content: userMessage,
+      },
+    ],
+  });
+
+  const aiResponse = completion.choices[0].message.content;
+
+  if (!aiResponse) {
+    throw new Error("Failed to generate AI response");
+  }
+
+  console.log("aiResponse", aiResponse);
+
+  return {
+    position: aiResponse.match(/Job title: (.*)/)?.[1] || "",
+    company: aiResponse.match(/Company: (.*)/)?.[1] || "",
+    description: (aiResponse.match(/Description:([\s\S]*)/)?.[1] || "").trim(),
+    startDate: aiResponse.match(/Start date: (\d{4}-\d{2}-\d{2})/)?.[1],
+    endDate: aiResponse.match(/End date: (\d{4}-\d{2}-\d{2})/)?.[1],
+  } satisfies WorkExperience;
 }

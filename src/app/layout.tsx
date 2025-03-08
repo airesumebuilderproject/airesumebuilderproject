2c9778e9adbfb0b4c8d4b0f8b392fb36b47037cd
airesumebuilderproject <airesumebuilderproject@gmail.com>
2025-03-08 23:48:50 +0530

    New Changes For Update For Hydration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,13 @@
+"use client"; // 👈 Isko Add Karo
+
 import { Toaster } from "@/components/ui/toaster";
 import { ClerkProvider } from "@clerk/nextjs";
-import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: {
-    template: "%s - AI Resume Builder",
-    absolute: "AI Resume Builder",
-  },
-  description:
-    "AI Resume Builder is the easiest way to create a professional resume that will help you land your dream job.",
-};
 
 export default function RootLayout({
   children,
